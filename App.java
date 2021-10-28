e905610f690fbfc3376e5c44569d868844ae82ac
Carlos <carlosfv2002@gmail.com>
2021-10-28 14:14:12 +0200

    Modificados archivos app y readme

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Carlos");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("28/10/2021");
+    System.out.println("Adios");
   }
   
 }
